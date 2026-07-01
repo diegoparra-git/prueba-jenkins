3591630d560217152173125acf9da4383216cd71
Jenkins Automator <jenkins@local>
2026-07-01 22:56:51 +0000

    docs/sec: Actualización automática del pipeline

---
+++ docs/html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['requests_0',['REQUESTS',['../namespaceapp.html#a6a85b8262aa5e4e35eacf9238df16482',1,'app']]]
+];
---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['buscar_0',['buscar',['../namespaceapp.html#ac20a8abb51bbccd2482fa88c6e183ba5',1,'app']]]
+];
